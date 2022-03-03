54263747459edcf7519a0401bf0268ac067b93ef
Rohit1144 <rohitchowdary1144@gmail.com>
2022-03-03 09:51:26 +0530

    functions and springdemo

---
+++ functions and springdemo/ManyToOne/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import logic.Service;
+
+public class Main {
+	public static void main(String args[]) {
+		Service serve = new Service();
+		serve.show();
+		System.out.println("Done");
+		
+	}
+
+}
